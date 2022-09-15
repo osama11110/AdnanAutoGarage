ae39e9a2545e6635900bb3c4e2dc50af5196d610
osama11110 <osamamalik1111@gmail.com>
2022-09-15 23:17:06 +0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from "@angular/router";
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OffersComponent } from './offers/offers.component';
+
+const appRoutes : Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'services', component: ServicesComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'contact-us', component: ContactUsComponent},
+  {path: 'offers', component: OffersComponent},
+  { path: '', redirectTo:'/home', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    OffersComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy',scrollPositionRestoration: 'enabled' })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
